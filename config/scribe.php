ff12da8d3fdeb1fe9ccfad690a1032c9522b1099
Ayodeji Adekunle <ayodejiadekunle@gmail.com>
2026-01-11 21:54:42 +0100

    fix render: remove invalid scribe class import

--- config/scribe.php
+++ config/scribe.php
@@ -2,7 +2,6 @@
 
 use Knuckles\Scribe\Extracting\Strategies;
 use Knuckles\Scribe\Config\Defaults;
-use Knuckles\Scribe\Config\AuthIn;
 use function Knuckles\Scribe\Config\{removeStrategies, configureStrategy};
 
 // Only the most common configs are shown. See the https://scribe.knuckles.wtf/laravel/reference/config for all.
